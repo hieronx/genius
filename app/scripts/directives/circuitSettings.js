c7b9937baaa435e90df7174dd857983c7a248614
Jeroen Offerijns <jeroen@offerijns.nl>
2014-05-16 16:37:56 +0200

    Remove incorrect code from circuit settings directive

--- app/scripts/directives/circuitSettings.js
+++ app/scripts/directives/circuitSettings.js
@@ -35,27 +35,6 @@ angular.module('geniusApp').directive('circuitSettings', function($rootScope) {
         isSource:true,
         isTarget:true  
       };
-
-      
-      jsPlumb.bind("click", function(c) {
-              var p = $(c.canvas).find("path"),
-                  l = p[0].getTotalLength(),
-                  i = l, d = -10, s = 10,
-                  att = function(a, v) {
-                      for (var i = 0; i < p.length; i++)
-                          p[i].setAttribute(a, v);
-                  },
-                  tick = function() {
-                      if (i > 0) {
-                          i += d;
-                          att("stroke-dashoffset", i);
-                          window.setTimeout(tick, s);
-                      }
-                  };
-              att("stroke-dasharray", l + " " + l);
-              tick();
-
-          });
     }
   };
 });
