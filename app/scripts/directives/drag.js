e6bef8dc3e4fb7df2a20c0324ece08f22b8daad1
Jeroen Offerijns <jeroen@offerijns.nl>
2014-05-16 11:55:34 +0200

    Remove old directives and controller code

--- app/scripts/directives/drag.js
+++
@@ -1,9 +0,0 @@
-// angular.module("geniusApp").directive('isDraggable', function() {
-//   return {
-//     restrict: 'A',
-//     link: function(scope, elm, attrs) {
-//       var options = scope.$eval(attrs.isDraggable); //allow options to be passed in
-//       elm.draggable(options);
-//     }
-//   };
-// });
