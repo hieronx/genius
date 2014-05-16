7c447d664fe93dedcce539997b4285615c390cce
vliegenthart <daniel.vliegenthart@gmail.com>
2014-05-16 12:11:53 +0200

    Add directives for droppable and jsplumb to main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -79,3 +79,113 @@ angular.module('geniusApp').controller('MainCtrl', function TodoCtrl($scope, $lo
     });
   };
 });
+
+angular.module('geniusApp').directive('isDraggable', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      var options = scope.$eval(attrs.isDraggable); //allow options to be passed in
+      elm.draggable(options);
+    }
+  };
+});
+
+angular.module('geniusApp').directive('isDroppable', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      var options = scope.$eval(attrs.isDraggable); //allow options to be passed in
+      var index = 1;
+      elm.droppable({
+        drop: function (event, ui) {
+
+          var $canvas = $(this);
+
+          if (!ui.draggable.hasClass('canvas-element')) {
+            var $canvasElement = ui.draggable.clone();
+            $canvasElement.addClass('canvas-element');
+            $canvasElement.draggable({
+              containment: '#workspace'
+            });
+            $canvasElement.addClass('brick-jsplumb').attr('id', 'brick-' + index);
+            index++;
+            $canvas.append($canvasElement);
+
+            var $par = $canvas.parent();
+            $canvasElement.css({
+              left: (ui.position.left + $par.outerWidth() + $par.position().left + 'px'),
+              top: (ui.position.top),
+              position: 'absolute'
+            });
+            $canvasElement.draggable( "destroy" );
+
+            jsPlumb.addEndpoint($canvasElement.attr('id'), { anchor:[0, 0.2, -1, 0] }, sourceEndpoint);
+            jsPlumb.addEndpoint($canvasElement.attr('id'), { anchor:[0, 0.8, -1, 0] }, sourceEndpoint);
+            jsPlumb.addEndpoint($canvasElement.attr('id'), { anchor:[1, 0.5, 0, 0] }, targetEndpoint);  
+
+            jsPlumb.draggable($canvasElement);
+          }
+        }
+      });
+    }
+  };
+});
+
+angular.module('geniusApp').directive('jsPlumb', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      var options = scope.$eval(attrs.isDraggable); //allow options to be passed in
+      jsPlumb.Defaults.Container = $('#workspace');
+  
+      connectorPaintStyle = {
+        lineWidth:4,
+        strokeStyle:"#61B7CF",
+        joinstyle:"round",
+        outlineColor:"transparent"
+      };
+
+      // .. and this is the hover style. 
+      connectorHoverStyle = {
+        lineWidth:4,
+        strokeStyle:"#216477",
+        outlineColor:"transparent"
+      };
+
+      endpointHoverStyle = {
+        fillStyle:"#216477",
+        strokeStyle:"#216477"
+      };
+
+      // the definition of source endpoints (the small blue ones)
+      sourceEndpoint = {
+        endpoint:"Dot",
+        paintStyle:{ 
+          strokeStyle:"#7AB02C",
+          fillStyle:"transparent",
+          radius:7,
+          lineWidth:3 
+        },        
+        isSource:true,
+        isTarget:true,
+        connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],                                
+        connectorStyle:connectorPaintStyle,
+        hoverPaintStyle:endpointHoverStyle,
+        connectorHoverStyle:connectorHoverStyle
+      };
+
+      targetEndpoint = {
+        endpoint:"Dot",         
+        paintStyle:{ fillStyle:"#7AB02C",radius:11 },
+        maxConnections: -1,
+        connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],                                
+        connectorStyle:connectorPaintStyle,
+        hoverPaintStyle:endpointHoverStyle,
+        connectorHoverStyle:connectorHoverStyle,
+        isSource:true,
+        isTarget:true  
+      };
+    }
+  };
+});
+
