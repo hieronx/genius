2d374998f342b39e13ed3e8152d7e275aa755f9e
Jeroen Offerijns <jeroen@offerijns.nl>
2014-05-16 12:45:23 +0200

    Separate directives into separate files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,4 +17,3 @@ angular
         redirectTo: '/'
       });
   })
-  
--- app/scripts/directives/isDraggable.js
+++ app/scripts/directives/isDraggable.js
@@ -1,13 +1,11 @@
 'use strict'
 
-var App = angular.module('geniusApp', []);
-
-App.directive('isDraggable', function() {
+angular.module('geniusApp').directive('isDraggable', function() {
   return {
     restrict: 'A',
-    link: function(scope, elm, attrs) {
-      var options = scope.$eval(attrs.isDraggable); //allow options to be passed in
-      elm.draggable(options);
+    link: function(scope, element, attributes) {
+      var options = scope.$eval(attributes.isDraggable); //allow options to be passed in
+      element.draggable(options);
     }
   };
 });
