758bfa35ce5d37d5240413e422dbd94bcb47ca52
Richard Machielse <richardmachielse@gmail.com>
2014-05-23 11:38:01 +0200

    fix includes

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -9,11 +9,16 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'app/bower_components/jquery/dist/jquery.min.js',
+      'app/bower_components/jquery/dist/jquery.js',
+      'bower_components/hoodie/dist/hoodie.js',
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/bower_components/underscore/underscore.js',
       'app/scripts/*.coffee',
       'app/scripts/**/*.coffee',
       'app/scripts/**/*.js',
