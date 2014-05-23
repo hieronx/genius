53c465f2de839266e46a0fa0db61f8eb32520d2d
Jeroen Offerijns <jeroen@offerijns.nl>
2014-05-23 09:39:44 +0200

    Implement test configurations

---
+++ test/e2e/auth/signin_spec.js
@@ -0,0 +1,16 @@
+describe('GENius', function() {
+
+  describe('Authentication sign in', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+
+    it('should log in as user completes the login form', function() {
+
+      browser.debugger();
+      
+    });
+  });
+});
